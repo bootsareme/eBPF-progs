0dc5019a467302cacc102a01d0c39a497ac160e4
Vincent Zhang <73851560+bootsareme@users.noreply.github.com>
2026-04-07 10:06:44 -0400

    Fix blocked IP handling in XDP firewall demo

--- kern/firewall_demo.bpf.c
+++ kern/firewall_demo.bpf.c
@@ -1,27 +1,19 @@
 /*
     This program blocks your machine from contacting BLOCKED_IP using XDP.
-    It is attached to your primary network interface and does filtering there.
+    It is attached to your network interface of choosing and does filtering there.
 */
 
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
-#include <bcc/helpers.h>
 
-typedef unsigned char byte;
-
-// union means addr will be filled out by the octets as they occupy same memory
-static const union {
-    uint32_t addr;
-    struct {
-        byte octet1;
-        byte octet2;
-        byte octet3;
-        byte octet4;
-    };
+static const struct {
+    uint8_t octet1;
+    uint8_t octet2;
+    uint8_t octet3;
+    uint8_t octet4;
 } BLOCKED_IP = {1, 0, 0, 1}; // 1.0.0.1 as an example
 
-SEC("xdp/firewall")
 int fw_block_ip(struct xdp_md *ctx)
 {
     void *frame_end = (void *)(long)ctx->data_end; // pointer to end of ethernet frame
@@ -33,16 +25,19 @@ int fw_block_ip(struct xdp_md *ctx)
 
     // if an IP packet is inside
     if (eth_header->h_proto == __constant_htons(ETH_P_IP)) {
-        struct iphdr *ip_header = data + sizeof(*eth);
+        struct iphdr *ip_header = frame + sizeof(*eth_header); // index past ethernet header
         if ((void *)ip_header + sizeof(*ip_header) > frame_end) // IP header too large!
             return XDP_PASS;
 
+        uint32_t blocked_ip = (BLOCKED_IP.octet1 << 24) |
+                                (BLOCKED_IP.octet2 << 16) |
+                                (BLOCKED_IP.octet3 << 8) |
+                                BLOCKED_IP.octet4;
+
         // if destination is the blocked IP, then drop the packet
-        if (ip->daddr == __constant_htonl(BLOCKED_IP.addr))
+        if (ip_header->daddr == __constant_htonl(blocked_ip))
             return XDP_DROP;
     }
 
     return XDP_PASS;
 }
-
-char LICENSE[] SEC("license") = "MIT";
